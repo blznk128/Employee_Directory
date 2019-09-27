73de59bea85ee002416b733828068da0b446ec5f
Benny Borunda <blznk128@yahoo.com>
2019-09-26 22:08:26 -0600

    started project

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const PORT = process.env.PORT || 8080;
+const db = require("./models");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static("public"));
+
+//routes
+// require("./routes/apiRoute")(app);
+
+db.sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => {
+        console.log("App is on port: " + PORT);
+    });
+});
