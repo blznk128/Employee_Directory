bad81de88925e3bec86d3b2b39d33399740115b0
Benny Borunda <blznk128@yahoo.com>
2019-09-30 17:57:21 -0600

    added models

---
+++ models/employee.js
@@ -0,0 +1,25 @@
+module.exports = (sequelize, DataTypes) => {
+    let Employee = sequelize.define("Employee", {
+        first_Name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        last_Name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        wage: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+        department: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        hire_Date: {
+            type: DataTypes.TEXT
+        }
+    });
+
+    return Employee;
+}
